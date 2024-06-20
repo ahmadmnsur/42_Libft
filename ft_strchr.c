f03c967fb3423c278d93407042a8090aec9612b4
ahmadmnsur <madrid7.am79@gmail.com>
2024-06-21 02:24:13 +0300

    edit ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -17,13 +17,13 @@ char	*ft_strchr(const char *str, int c)
 	char	ch;
 
 	ch = (char) c;
+	if (ch == '\0')
+		return ((char *) str);
 	while (*str)
 	{
 		if (*str == ch)
 			return ((char *) str);
 		str++;
 	}
-	if (ch == '\0')
-		return ((char *) str);
 	return (NULL);
 }
